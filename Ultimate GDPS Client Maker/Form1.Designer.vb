3168e41ef447f96bed58aafc07e8776cd6cc8b5c
Jean <63162857+masckmaster2007@users.noreply.github.com>
2025-03-03 18:30:06 +0100

    Add project files.

---
+++ Ultimate GDPS Client Maker/Form1.Designer.vb
@@ -0,0 +1,130 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(13, 31)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(225, 20)
+        Me.TextBox1.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(244, 30)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Open"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(13, 13)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(287, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "File (GeometryDash.exe, GeometryJump, libcocos2dcpp.so)"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(12, 83)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(306, 36)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "Run"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(12, 126)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.TextBox2.Size = New System.Drawing.Size(306, 166)
+        Me.TextBox2.TabIndex = 4
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(74, 57)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(244, 20)
+        Me.TextBox3.TabIndex = 5
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 60)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(56, 13)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "GDPS Url:"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(333, 301)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximumSize = New System.Drawing.Size(349, 340)
+        Me.MinimumSize = New System.Drawing.Size(349, 340)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Ultimate GDPS Client Maker"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label2 As Label
+End Class
---
+++ Ultimate GDPS Client Maker/Form1.vb
@@ -0,0 +1,79 @@
+﻿Imports System.IO
+
+Public Class Form1
+
+    Private Sub PrintLog(Log As String)
+        TextBox2.Text += Log + Environment.NewLine
+    End Sub
+    Private Function IsWindows(FPath As String)
+        Return FPath.EndsWith(".exe")
+    End Function
+    Private Function IsAndroid(FPath As String)
+        Return FPath.EndsWith(".so")
+    End Function
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If OpenFileDialog1.ShowDialog() = DialogResult.OK Then
+            TextBox1.Text = OpenFileDialog1.FileName
+            Select Case True
+                Case IsWindows(OpenFileDialog1.FileName)
+                    PrintLog("Detected: Windows")
+                Case IsAndroid(OpenFileDialog1.FileName)
+                    PrintLog("Detected: Android")
+                Case Else
+                    PrintLog("Detected: iOS")
+            End Select
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        If TextBox3.Text.Length > 33 Then
+            PrintLog("URL length is too long! Please use a shorter one!")
+        End If
+
+        While TextBox3.Text.Length <> 33
+            TextBox3.Text += "/"
+        End While
+
+        PrintLog("Replacing...")
+
+        Dim BinaryBytes As Byte() = File.ReadAllBytes(TextBox1.Text)
+        Dim Binary As String = System.Text.Encoding.Default.GetString(BinaryBytes)
+        ' Basic Replacing
+        Binary = Binary.Replace("http://www.boomlings.com/database", TextBox3.Text)
+        Binary = Binary.Replace("https://www.boomlings.com/database", TextBox3.Text + "/")
+        Binary = Binary.Replace(Convert.ToBase64String(System.Text.Encoding.Default.GetBytes("http://www.boomlings.com/database")), Convert.ToBase64String(System.Text.Encoding.Default.GetBytes(TextBox3.Text)))
+
+        If Not IsWindows(TextBox1.Text) Then
+            Dim MaxBundleLength As Integer = 23
+            Dim OriginalBundle = "com.robtop.geometryjump"
+            If IsAndroid(TextBox1.Text) Then
+                PrintLog("Android detected, changing bundle ID to Android's")
+                MaxBundleLength += 1
+                OriginalBundle = "com.robtopx.geometryjump"
+            Else
+                PrintLog("iOS detected, not changing original bundle ID")
+            End If
+            Dim BundleName = InputBox("Enter Bundle ID (com.example.mygdps). Must be " + MaxBundleLength + " characters long!")
+            If BundleName.Length > MaxBundleLength Then
+                PrintLog("Length too long! Please make it shorter.")
+                Return
+            End If
+            While BundleName.Length <> MaxBundleLength
+                BundleName += "0"
+            End While
+            Binary = Binary.Replace(OriginalBundle, BundleName)
+        End If
+
+        BinaryBytes = System.Text.Encoding.Default.GetBytes(Binary)
+
+        If IsWindows(TextBox1.Text) Then
+            PrintLog("Windows detected: Asking for GDPS name")
+            Dim TargetName = InputBox("Enter your GDPS name:")
+            File.WriteAllBytes(Path.Combine(Directory.GetParent(TextBox1.Text).ToString, TargetName & ".exe"), BinaryBytes)
+        Else
+            File.WriteAllBytes(TextBox1.Text, BinaryBytes)
+        End If
+        PrintLog("GDPS Client created! Enjoy!")
+    End Sub
+End Class
