8e5da96d75d96f8e91c7832ec0700092c45cd20f
Jean <63162857+masckmaster2007@users.noreply.github.com>
2025-03-21 20:20:58 +0100

    balls hd

--- Ultimate GDPS Client Maker/Form1.Designer.vb
+++ Ultimate GDPS Client Maker/Form1.Designer.vb
@@ -35,16 +35,18 @@ Partial Class Form1
         '
         'TextBox1
         '
-        Me.TextBox1.Location = New System.Drawing.Point(13, 31)
+        Me.TextBox1.Location = New System.Drawing.Point(17, 38)
+        Me.TextBox1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(225, 20)
+        Me.TextBox1.Size = New System.Drawing.Size(299, 22)
         Me.TextBox1.TabIndex = 0
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(244, 30)
+        Me.Button1.Location = New System.Drawing.Point(325, 37)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.Size = New System.Drawing.Size(100, 28)
         Me.Button1.TabIndex = 1
         Me.Button1.Text = "Open"
         Me.Button1.UseVisualStyleBackColor = True
@@ -52,28 +54,31 @@ Partial Class Form1
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(13, 13)
+        Me.Label1.Location = New System.Drawing.Point(17, 16)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(287, 13)
+        Me.Label1.Size = New System.Drawing.Size(398, 16)
         Me.Label1.TabIndex = 2
-        Me.Label1.Text = "File (GeometryDash.exe, GeometryJump, libcocos2dcpp.so)"
+        Me.Label1.Text = "File (GeometryDash.exe, GeometryDash.ipa, GeometryDash.apk)"
         '
         'Button2
         '
-        Me.Button2.Location = New System.Drawing.Point(12, 83)
+        Me.Button2.Location = New System.Drawing.Point(16, 102)
+        Me.Button2.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(306, 36)
+        Me.Button2.Size = New System.Drawing.Size(408, 44)
         Me.Button2.TabIndex = 3
         Me.Button2.Text = "Run"
         Me.Button2.UseVisualStyleBackColor = True
         '
         'TextBox2
         '
-        Me.TextBox2.Location = New System.Drawing.Point(12, 126)
+        Me.TextBox2.Location = New System.Drawing.Point(16, 155)
+        Me.TextBox2.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.TextBox2.Multiline = True
         Me.TextBox2.Name = "TextBox2"
         Me.TextBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
-        Me.TextBox2.Size = New System.Drawing.Size(306, 166)
+        Me.TextBox2.Size = New System.Drawing.Size(407, 203)
         Me.TextBox2.TabIndex = 4
         '
         'OpenFileDialog1
@@ -82,25 +87,27 @@ Partial Class Form1
         '
         'TextBox3
         '
-        Me.TextBox3.Location = New System.Drawing.Point(74, 57)
+        Me.TextBox3.Location = New System.Drawing.Point(99, 70)
+        Me.TextBox3.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.TextBox3.Name = "TextBox3"
-        Me.TextBox3.Size = New System.Drawing.Size(244, 20)
+        Me.TextBox3.Size = New System.Drawing.Size(324, 22)
         Me.TextBox3.TabIndex = 5
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(12, 60)
+        Me.Label2.Location = New System.Drawing.Point(16, 74)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(56, 13)
+        Me.Label2.Size = New System.Drawing.Size(68, 16)
         Me.Label2.TabIndex = 6
         Me.Label2.Text = "GDPS Url:"
         '
         'Form1
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(333, 301)
+        Me.ClientSize = New System.Drawing.Size(441, 361)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.TextBox3)
         Me.Controls.Add(Me.TextBox2)
@@ -109,8 +116,9 @@ Partial Class Form1
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.TextBox1)
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
-        Me.MaximumSize = New System.Drawing.Size(349, 340)
-        Me.MinimumSize = New System.Drawing.Size(349, 340)
+        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.MaximumSize = New System.Drawing.Size(459, 408)
+        Me.MinimumSize = New System.Drawing.Size(459, 408)
         Me.Name = "Form1"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Ultimate GDPS Client Maker"
--- Ultimate GDPS Client Maker/Form1.vb
+++ Ultimate GDPS Client Maker/Form1.vb
@@ -1,34 +1,96 @@
 ﻿Imports System.IO
-
+Imports System.IO.Compression
+Imports System.Net
 Public Class Form1
-
+    ' Top 10 Spaghetti Code
     Private Sub PrintLog(Log As String)
         TextBox2.Text += Log + Environment.NewLine
     End Sub
-    Private Function IsWindows(FPath As String)
-        Return FPath.EndsWith(".exe")
-    End Function
-    Private Function IsAndroid(FPath As String)
-        Return FPath.EndsWith(".so")
+    Private Function Platform(FPath As String) As Integer
+        ' 1 => Windows
+        ' 2 => iOS
+        ' 3 => Android
+        If FPath.EndsWith(".exe") Then
+            Return 1
+        ElseIf FPath.EndsWith(".ipa") Then
+            Return 2
+        ElseIf FPath.EndsWith(".apk") Then
+            Return 3
+        Else Return 0
+        End If
     End Function
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         If OpenFileDialog1.ShowDialog() = DialogResult.OK Then
             TextBox1.Text = OpenFileDialog1.FileName
-            Select Case True
-                Case IsWindows(OpenFileDialog1.FileName)
+            Select Case Platform(OpenFileDialog1.FileName)
+                Case 1
                     PrintLog("Detected: Windows")
-                Case IsAndroid(OpenFileDialog1.FileName)
+                Case 2
+                    PrintLog("Detected: iOS")
+                Case 3
                     PrintLog("Detected: Android")
                 Case Else
-                    PrintLog("Detected: iOS")
+                    PrintLog("Detected: No OS")
             End Select
         End If
     End Sub
 
+    Private Sub ReplaceBinary(BinaryPath As String, PSUrl As String, BundleName As String, PSName As String, Optional NewBinaryPC As String = "")
+        Dim BinaryBytes As Byte() = File.ReadAllBytes(BinaryPath)
+        Dim Binary As String = System.Text.Encoding.Default.GetString(BinaryBytes)
+        ' Basic Replacing
+        Binary = Binary.Replace("http://www.boomlings.com/database", PSUrl)
+        Binary = Binary.Replace("https://www.boomlings.com/database", PSUrl + "/")
+        Binary = Binary.Replace(Convert.ToBase64String(System.Text.Encoding.Default.GetBytes("http://www.boomlings.com/database")), Convert.ToBase64String(System.Text.Encoding.Default.GetBytes(PSUrl)))
+        If Not String.IsNullOrEmpty(BundleName) Then
+            Dim BundleOriginalName = "com.robtopx.geometryjump"
+            If BundleName.Length = 23 Then
+                ' iOS
+                BundleOriginalName = "com.robtop.geometryjump"
+                If BinaryPath.EndsWith(".plist") Then
+                    Binary = Binary.Replace("Geometry", $"{PSName}")
+                    ' I am Lazy :)
+                    Binary = Binary.Replace($"{PSName}Jump", "GeometryJump")
+                End If
+            End If
+            Binary = Binary.Replace(BundleOriginalName, BundleName)
+        End If
+        BinaryBytes = System.Text.Encoding.Default.GetBytes(Binary)
+        If String.IsNullOrEmpty(NewBinaryPC) Then
+            File.WriteAllBytes(BinaryPath, BinaryBytes)
+        Else
+            File.WriteAllBytes(NewBinaryPC, BinaryBytes)
+        End If
+    End Sub
+
+    Private Function StartAndWait(AppPath As String, Arguments As String)
+        Dim P As New ProcessStartInfo
+        P.FileName = AppPath
+        P.Arguments = Arguments
+        P.UseShellExecute = False
+        P.RedirectStandardError = True
+        P.CreateNoWindow = True
+        Dim Hi = Process.Start(P)
+        If Hi IsNot Nothing Then
+            Hi.WaitForExit()
+        Else
+            MsgBox("Failed to start process: " & AppPath)
+        End If
+        Return Hi.ExitCode
+        ' no more sigma MsgBox(Hi.StandardError.ReadLine())
+    End Function
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        PrintLog("Asking for GDPS name")
+        Dim TargetName = InputBox("Enter your GDPS name:")
+
+        If String.IsNullOrEmpty(TargetName) Then
+            Return
+        End If
 
         If TextBox3.Text.Length > 33 Then
             PrintLog("URL length is too long! Please use a shorter one!")
+            Return
         End If
 
         While TextBox3.Text.Length <> 33
@@ -37,24 +99,20 @@ Public Class Form1
 
         PrintLog("Replacing...")
 
-        Dim BinaryBytes As Byte() = File.ReadAllBytes(TextBox1.Text)
-        Dim Binary As String = System.Text.Encoding.Default.GetString(BinaryBytes)
-        ' Basic Replacing
-        Binary = Binary.Replace("http://www.boomlings.com/database", TextBox3.Text)
-        Binary = Binary.Replace("https://www.boomlings.com/database", TextBox3.Text + "/")
-        Binary = Binary.Replace(Convert.ToBase64String(System.Text.Encoding.Default.GetBytes("http://www.boomlings.com/database")), Convert.ToBase64String(System.Text.Encoding.Default.GetBytes(TextBox3.Text)))
+        Dim BundleName = ""
 
-        If Not IsWindows(TextBox1.Text) Then
-            Dim MaxBundleLength As Integer = 23
-            Dim OriginalBundle = "com.robtop.geometryjump"
-            If IsAndroid(TextBox1.Text) Then
-                PrintLog("Android detected, changing bundle ID to Android's")
+        If Platform(TextBox1.Text) > 1 Then
+            Dim MaxBundleLength As Double = 23
+            If Platform(TextBox1.Text) = 3 Then
+                PrintLog("Android detected, incrementing bundle ID length to 24")
                 MaxBundleLength += 1
-                OriginalBundle = "com.robtopx.geometryjump"
             Else
-                PrintLog("iOS detected, not changing original bundle ID")
+                PrintLog("iOS detected, keeping bundle ID length")
             End If
-            Dim BundleName = InputBox("Enter Bundle ID (com.example.mygdps). Must be " + MaxBundleLength + " characters long!")
+            BundleName = InputBox("Enter Bundle ID (com.example.mygdps). Must be " & MaxBundleLength.ToString & " characters long!")
+            If String.IsNullOrEmpty(BundleName) Then
+                Return
+            End If
             If BundleName.Length > MaxBundleLength Then
                 PrintLog("Length too long! Please make it shorter.")
                 Return
@@ -62,18 +120,74 @@ Public Class Form1
             While BundleName.Length <> MaxBundleLength
                 BundleName += "0"
             End While
-            Binary = Binary.Replace(OriginalBundle, BundleName)
         End If
 
-        BinaryBytes = System.Text.Encoding.Default.GetBytes(Binary)
+        If Platform(TextBox1.Text) = 2 Then
+            Dim DindeIOS = Path.Combine(Environment.CurrentDirectory, "dindetemp_ios")
+            PrintLog("Extracting IPA")
+            ZipFile.ExtractToDirectory(TextBox1.Text, DindeIOS)
+            Dim BinaryPath = Path.Combine(DindeIOS, "Payload", "GeometryJump.app", "GeometryJump")
+            Dim PlistPath = Path.Combine(DindeIOS, "Payload", "GeometryJump.app", "Info.plist")
+            ReplaceBinary(BinaryPath, TextBox3.Text, BundleName, TargetName)
+            ReplaceBinary(PlistPath, TextBox3.Text, BundleName, TargetName)
+            PrintLog("Exporting IPA")
+            ZipFile.CreateFromDirectory(DindeIOS, $"{TargetName}.ipa", CompressionLevel.NoCompression, False)
+            Directory.Delete(DindeIOS, True)
+        ElseIf Platform(TextBox1.Text) = 1 Then
+            Dim TargetBinaryPath = Path.Combine(Directory.GetParent(TextBox1.Text).ToString, TargetName & ".exe")
+            ReplaceBinary(TextBox1.Text, TextBox3.Text, "", TargetName, TargetBinaryPath)
+        ElseIf Platform(TextBox1.Text) = 3 Then
+            Dim JavaCheck = StartAndWait("where", "java")
+            If JavaCheck <> 0 Then
+                PrintLog("Java is not detected! If it is, go to Environment Variables on windows search and edit the Path variable to include it!")
+                Return
+            End If
+            Dim ApkTool = Path.Combine(Environment.CurrentDirectory, "apktool")
+            PrintLog("Extracting APK")
+            If Not Directory.Exists(ApkTool) Then
+                PrintLog("APKTool not present, downloading...")
+                ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls Or SecurityProtocolType.Tls11 Or SecurityProtocolType.Tls12
 
-        If IsWindows(TextBox1.Text) Then
-            PrintLog("Windows detected: Asking for GDPS name")
-            Dim TargetName = InputBox("Enter your GDPS name:")
-            File.WriteAllBytes(Path.Combine(Directory.GetParent(TextBox1.Text).ToString, TargetName & ".exe"), BinaryBytes)
+                Dim remoteUri As String = "https://files.141412.xyz/r/apktool.zip"
+                Dim fileName As String = "apktool.zip"
+
+                Using client As New WebClient()
+                    client.DownloadFile(remoteUri, fileName)
+                End Using
+
+                ZipFile.ExtractToDirectory(Path.Combine(Environment.CurrentDirectory, "apktool.zip"), Environment.CurrentDirectory)
+                File.Delete(Path.Combine(Environment.CurrentDirectory, "apktool.zip"))
+            End If
+            StartAndWait("java", $"-jar apktool\apktool.jar d ""{TextBox1.Text}"" -o apkedit")
+            ' Inshallah
+            Dim File1 = Path.Combine(Environment.CurrentDirectory, "apkedit", "res", "values", "strings.xml")
+            Dim File2 = Path.Combine(Environment.CurrentDirectory, "apkedit", "apktool.yml")
+            Dim Path1 = Directory.GetDirectories(Path.Combine(Environment.CurrentDirectory, "apkedit", "lib"))
+            For Each Dir As String In Path1
+                If File.Exists(Path.Combine(Dir, "libcocos2dcpp.so")) Then
+                    ReplaceBinary(Path.Combine(Dir, "libcocos2dcpp.so"), TextBox3.Text, BundleName, TargetName)
+                ElseIf File.Exists(Path.Combine(Dir, "libgame.so")) Then
+                    ReplaceBinary(Path.Combine(Dir, "libgame.so"), TextBox3.Text, BundleName, TargetName)
+                End If
+            Next
+            File.WriteAllText(File1, File.ReadAllText(File1).Replace("Geometry Dash", TargetName))
+            File.WriteAllText(File2, File.ReadAllText(File2).Replace("renameManifestPackage: null", $"renameManifestPackage: {BundleName}"))
+            StartAndWait("java", $"-jar apktool\apktool.jar b apkedit -o build_temp.apk")
+            StartAndWait("apktool\zipalign.exe", $"4 build_temp.apk ""{TargetName}.apk""")
+            File.Delete(Path.Combine(Environment.CurrentDirectory, "build_temp.apk"))
+            StartAndWait("java", $"-jar apktool\apksigner.jar sign --ks apktool\demo.jks --ks-pass pass:123456 ""{TargetName}.apk""")
+            Directory.Delete(Path.Combine(Environment.CurrentDirectory, "apkedit"), True)
         Else
-            File.WriteAllBytes(TextBox1.Text, BinaryBytes)
+            PrintLog("No platform detected")
+            Return
         End If
+
         PrintLog("GDPS Client created! Enjoy!")
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        PrintLog("Discord: https://dimisaio.be/discord")
+        PrintLog("Note: Edited APKs/IPAs will be saved to the same folder as this program!")
+        PrintLog("Note 2: Do not click on the program while doing its work! It will freeze and crash!")
     End Sub
 End Class
